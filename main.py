a7cccde9c06be62a2d0bdf7bf1e05785be5db1dd
Allexndr <85500726+Allexndr@users.noreply.github.com>
2022-12-21 11:15:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ id_sticker = "CAACAgIAAxkBAAEGlkVjhKdcurpQwMm9redbmkBGG1uUEwACKgwAArn2kUnBfIbAyJ
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     language_message = detect(message.text)
+    print(language_message)
     s_2 = []
     s_3 = []
+    s_4 = []
     if "/t" in message.text:
         if language_message == "ne":
             for i in message.text:
-                if i == "ी" or i == "्" or i == "ो" or i == "ा":
+                if i == "ी" or i == "्" or i == "ो" or i == "ा" or i =="ि" or i =="े" or i == "़":
                     s_3.append(i)
                 else:
                     s_3 += random.choice(uni_list)
@@ -24,6 +26,18 @@ def repeat_all_messages(message):
             del s_3[0:5]
             bot.send_message(message.chat.id, "".join(map(str, s_3)),
                              reply_to_message_id=message.message_id)
+        elif language_message == "hi":
+            for i in message.text:
+                if i == "ी" or i == "्" or i == "ो" \
+                        or i == "ा" or i == "ि" or i == "े" or i == "़" or i == "ै" or i == "ं":
+                    s_4.append(i)
+                else:
+                    s_4 += random.choice(uni_list)
+                    s_4.append(i)
+            del s_4[0:5]
+            bot.send_message(message.chat.id, "".join(map(str, s_4)),
+                             reply_to_message_id=message.message_id)
+            print(language_message)
         else:
             for i in message.text:
                 s_2.append(i)
@@ -38,3 +52,5 @@ def repeat_all_messages(message):
 
 if __name__ == '__main__':
     bot.infinity_polling()
+
+
